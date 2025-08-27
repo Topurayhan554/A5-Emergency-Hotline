1ce406b7f5788e6aa6931bc9ad14e3505e67bc05
Topurayhan554 <topurayhantipu@gmail.com>
2025-08-27 12:29:13 +0600

    added history js function

--- script.js
+++ script.js
@@ -15,3 +15,40 @@ getElement("cart-container").addEventListener("click", function (e) {
     console.log(countBtn);
   }
 });
+
+getElement("cart-container").addEventListener("click", function (e) {
+  if (e.target.className.includes("call-button")) {
+    const callBtn = e.target;
+
+    const serviceName =
+      callBtn.parentNode.parentNode.children[1].children[0].innerText;
+
+    const description =
+      callBtn.parentNode.parentNode.children[1].children[1].innerText;
+
+    const serviceNumber =
+      callBtn.parentNode.parentNode.children[2].children[0].innerText;
+
+    // const callIcon = callBtn.parentNode.children[1].children[0].src;
+    // console.log(callIcon);
+
+    alert(`emergency-service-netlify.app says
+  Calling ${serviceName}${serviceNumber}..`);
+
+    const historyContainer = getElement("history-container");
+
+    const div = document.createElement("div");
+    const date = new Date().toLocaleTimeString();
+
+    div.innerHTML = `<div
+                class="flex justify-between items-center bg-[#FAFAFA] p-2 rounded-lg mt-3"
+              >
+                <div class="text-lg">
+                  <h1 class="font-semibold">${serviceName}</h1>
+                  <p class="text-[#5C5C5C]">${serviceNumber}</p>
+                </div>
+                <span class="text-[#5c5c5c]">${date}</span>
+              </div>`;
+    historyContainer.append(div);
+  }
+});
